75837456a2d331c51ce0256fb5e7dc3d41233a82
Umesh <mathulkar98@gmail.com>
2023-01-08 02:55:52 -0800

    updated03

--- wirem.js
+++ wirem.js
@@ -1,15 +1,74 @@
-function productsadd(){
-    document.getElementById("products").classList.remove("hide");
-    document.getElementById("products").classList.add("landingProd")
-}
-function productsdel(){
-    document.getElementById("products").classList.remove("landingProd");
-    document.getElementById("products").classList.add("hide")
+
+//code shortning
+function modes(element){
+    if(element=="products"){
+        document.getElementById("products").classList.remove("hide");
+    document.getElementById("products01").classList.add("hide");       
+    }
+    else{
+       
+    document.getElementById("products").classList.add("hide");
+    document.getElementById("products01").classList.remove("hide");
+    }
+   
 }
 
-function weladd(){
-    document.getElementById("products01").classList.remove("hide");
-}
-function weldel(){
-    document.getElementById("products01").classList.add("hide")
+function backChange(element){
+    if(element=="cctv"){
+        document.getElementById("lands").classList.add("cctv");
+        document.getElementById("lands").classList.remove("door");
+        document.getElementById("lands").classList.remove("elect");
+        document.getElementById("lands").classList.remove("scan");
+        document.getElementById("lands").classList.remove("telephone");
+        document.getElementById("lands").classList.remove("wifi");
+    }
+    else if (element=="door"){
+        document.getElementById("lands").classList.add("door");
+        document.getElementById("lands").classList.remove("cctv");
+        document.getElementById("lands").classList.remove("elect");
+        document.getElementById("lands").classList.remove("scan");
+        document.getElementById("lands").classList.remove("telephone");
+        document.getElementById("lands").classList.remove("wifi");
+        
+    }
+    else if (element=="electric"){
+        document.getElementById("lands").classList.add("elect");
+        document.getElementById("lands").classList.remove("door");
+        document.getElementById("lands").classList.remove("cctv");
+        document.getElementById("lands").classList.remove("scan");
+        document.getElementById("lands").classList.remove("telephone");
+        document.getElementById("lands").classList.remove("wifi");
+    }
+    else if (element=="scan"){
+        document.getElementById("lands").classList.add("scan");
+        document.getElementById("lands").classList.remove("door");
+        document.getElementById("lands").classList.remove("elect");
+        document.getElementById("lands").classList.remove("cctv");
+        document.getElementById("lands").classList.remove("telephone");
+        document.getElementById("lands").classList.remove("wifi");
+    }
+    else if (element=="telephone"){
+        document.getElementById("lands").classList.add("telephone");
+        document.getElementById("lands").classList.remove("door");
+        document.getElementById("lands").classList.remove("elect");
+        document.getElementById("lands").classList.remove("scan");
+        document.getElementById("lands").classList.remove("cctv");
+        document.getElementById("lands").classList.remove("wifi");
+    }
+    else if (element=="wifi"){
+        document.getElementById("lands").classList.add("wifi");
+        document.getElementById("lands").classList.remove("door");
+        document.getElementById("lands").classList.remove("elect");
+        document.getElementById("lands").classList.remove("scan");
+        document.getElementById("lands").classList.remove("telephone");
+        document.getElementById("lands").classList.remove("cctv");
+    }
+    else if(element=="original"){
+        document.getElementById("lands").classList.remove("scan");
+        document.getElementById("lands").classList.remove("door");
+        document.getElementById("lands").classList.remove("elect");
+        document.getElementById("lands").classList.remove("cctv");
+        document.getElementById("lands").classList.remove("telephone");
+        document.getElementById("lands").classList.remove("wifi");
+    }
 }
