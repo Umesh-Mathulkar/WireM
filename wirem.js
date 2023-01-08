94800e2b6f974aa7de2db76e0e7248d4c1dee92a
Umesh <mathulkar98@gmail.com>
2023-01-08 03:09:06 -0800

    first

---
+++ wirem.js
@@ -0,0 +1,15 @@
+function productsadd(){
+    document.getElementById("products").classList.remove("hide");
+    document.getElementById("products").classList.add("landingProd")
+}
+function productsdel(){
+    document.getElementById("products").classList.remove("landingProd");
+    document.getElementById("products").classList.add("hide")
+}
+
+function weladd(){
+    document.getElementById("products01").classList.remove("hide");
+}
+function weldel(){
+    document.getElementById("products01").classList.add("hide")
+}
