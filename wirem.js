9c78b0ae3ceecc4509aa902391525dc5501a63da
Umesh <mathulkar98@gmail.com>
2023-01-11 03:15:28 -0800

    updated03

--- wirem.js
+++ wirem.js
@@ -1,5 +1,18 @@
 
 //code shortning
+function backChange(element){
+    document.getElementById("lands").classList.add(`${element}`);
+}
+function backChangeDel(element){
+    document.getElementById("lands").classList.remove(`${element}`);
+}
+
+
+function backChanges(entity){
+   
+}
+
+
 function modes(element) {
     if (element == "products") {
         document.getElementById("products").classList.remove("hide");
@@ -13,62 +26,9 @@ function modes(element) {
 
 };
 
-function backChange(element) {
-    if (element == "cctv") {
-        document.getElementById("lands").classList.add("cctv");
-        document.getElementById("lands").classList.remove("door");
-        document.getElementById("lands").classList.remove("elect");
-        document.getElementById("lands").classList.remove("scan");
-        document.getElementById("lands").classList.remove("telephone");
-        document.getElementById("lands").classList.remove("wifi");
-    }
-    else if (element == "door") {
-        document.getElementById("lands").classList.add("door");
-        document.getElementById("lands").classList.remove("cctv");
-        document.getElementById("lands").classList.remove("elect");
-        document.getElementById("lands").classList.remove("scan");
-        document.getElementById("lands").classList.remove("telephone");
-        document.getElementById("lands").classList.remove("wifi");
+const hamburger = document.querySelector('.hamburger');
+const navLink = document.querySelector('.nav__link');
 
-    }
-    else if (element == "electric") {
-        document.getElementById("lands").classList.add("elect");
-        document.getElementById("lands").classList.remove("door");
-        document.getElementById("lands").classList.remove("cctv");
-        document.getElementById("lands").classList.remove("scan");
-        document.getElementById("lands").classList.remove("telephone");
-        document.getElementById("lands").classList.remove("wifi");
-    }
-    else if (element == "scan") {
-        document.getElementById("lands").classList.add("scan");
-        document.getElementById("lands").classList.remove("door");
-        document.getElementById("lands").classList.remove("elect");
-        document.getElementById("lands").classList.remove("cctv");
-        document.getElementById("lands").classList.remove("telephone");
-        document.getElementById("lands").classList.remove("wifi");
-    }
-    else if (element == "telephone") {
-        document.getElementById("lands").classList.add("telephone");
-        document.getElementById("lands").classList.remove("door");
-        document.getElementById("lands").classList.remove("elect");
-        document.getElementById("lands").classList.remove("scan");
-        document.getElementById("lands").classList.remove("cctv");
-        document.getElementById("lands").classList.remove("wifi");
-    }
-    else if (element == "wifi") {
-        document.getElementById("lands").classList.add("wifi");
-        document.getElementById("lands").classList.remove("door");
-        document.getElementById("lands").classList.remove("elect");
-        document.getElementById("lands").classList.remove("scan");
-        document.getElementById("lands").classList.remove("telephone");
-        document.getElementById("lands").classList.remove("cctv");
-    }
-    else if (element == "original") {
-        document.getElementById("lands").classList.remove("scan");
-        document.getElementById("lands").classList.remove("door");
-        document.getElementById("lands").classList.remove("elect");
-        document.getElementById("lands").classList.remove("cctv");
-        document.getElementById("lands").classList.remove("telephone");
-        document.getElementById("lands").classList.remove("wifi");
-    }
-};
+hamburger.addEventListener('click', () => {
+  navLink.classList.toggle('hide');
+});
